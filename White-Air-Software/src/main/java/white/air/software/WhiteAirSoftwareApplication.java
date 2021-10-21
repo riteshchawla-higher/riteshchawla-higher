4060e0de4d0d32d39466dc2e9a70c3d514ff5014
riteshchawla-higher <riteshchawla.higher@gmail.com>
2021-10-21 13:06:59 +0530

    Initial Commit

---
+++ White-Air-Software/src/main/java/white/air/software/WhiteAirSoftwareApplication.java
@@ -0,0 +1,13 @@
+package white.air.software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteAirSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteAirSoftwareApplication.class, args);
+	}
+
+}
---
+++ White-Air-Software/src/test/java/white/air/software/WhiteAirSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package white.air.software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteAirSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
